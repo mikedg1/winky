38a7c2c9387c66de579c02024a82f0b38c4a9707
mdigiovanni <vinblau@gmail.com>
2013-05-01 22:28:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+winky
+===========
+
+A way to enable the wink gesture on Google Glass to take a photo.
+
+- The screen can be off while you wink, this means completely hands and mouth free photo taking. Take pictures while you eat!
+- Run the app, then use the touchpad to tap on the calibrate. Follow the on screen directions to calibrate.
+
+So why do we need this app?
+There's at least one location in code on Glass that does a check to see if the wink gesture should be enabled and then turns it off if it shouldn't. This tunrs on the wink gesture, and makes sure that Glass won't be able to disable it by intercepting any future wink gestures and responding with a photo.
+
+Who is Mike DiGiovanni? Emerging technology lead at Roundarch Isobar (http://www.roundarchisobar.com) Mike has interests in all areas of mobile development and wearable computing. As a long time Android developer, he is looking forward to working wiht Google Glass.
+
+Copyright 2013 Michael DiGiovanni glass@mikedg.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/com/mikedg/android/glass/winky/MainActivity.java
@@ -0,0 +1,133 @@
+/*
+Copyright 2013 Michael DiGiovanni glass@mikedg.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.mikedg.android.glass.winky;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainActivity extends Activity {
+
+    private static String TAG = "dgGestureService";
+    Object glassGestureManager;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //Get the string for the gesture service that we want
+        String str = getConstantFromClass("GLASS_GESTURE_SERVICE", Context.class);
+        if (str == null)
+        {
+            finish();
+        }
+        
+        glassGestureManager = this.getSystemService(str);
+    }
+
+    public void onClick_calibration(View view) {
+        //Disables the wink receiver while we calibrate because calibration won't receive a wink if we intercept it
+        disableWinkReceiver();
+        this.startActivity(new Intent("com.google.glass.action.ACTION_WINK_CALIBRATION"));
+    }
+    
+    private void disableWinkReceiver() {
+        setWinkReceiverEnabled(false);
+    }
+
+    private void setWinkReceiverEnabled(boolean b) {
+        ComponentName component = new ComponentName(this, EyeGestureReceiver.class);
+        getPackageManager().setComponentEnabledSetting(component,
+                b ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);        
+    }
+
+    private void enableWinkReceiver() {
+        setWinkReceiverEnabled(true);
+    }
+    
+    public void onClick_clearCalibration(View view) {
+        clearCalibation();
+        Toast.makeText(this, "Cleared calibration", Toast.LENGTH_SHORT).show();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        //We want to always enable wink detection when we resume
+        //Just easier to do it here in case you accidentally get it disabled by the system
+        //Calibrate should enable it by default though
+        enableWinkReceiver();
+        enableWinkDetection();
+    }
+    
+
+    public void clearCalibation() {
+        try {
+            Method method = glassGestureManager.getClass().getMethod("clearEyeCalibration");
+            method.invoke(glassGestureManager);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public void enableWinkDetection() {
+        try {
+            Method method = glassGestureManager.getClass().getMethod("enableWinkDetector", Boolean.TYPE);
+            method.invoke(glassGestureManager, true);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static String getConstantFromClass(String paramString, Class<?> paramClass) {
+        try {
+            String str = (String) paramClass.getDeclaredField(paramString).get(null);
+            return str;
+        } catch (IllegalAccessException localIllegalAccessException) {
+            Log.e(TAG, "Unable to get " + paramString + " from " + paramClass.getSimpleName(),
+                    localIllegalAccessException);
+            return null;
+        } catch (NoSuchFieldException localNoSuchFieldException) {
+            Log.e(TAG, "Unable to get " + paramString + " from " + paramClass.getSimpleName(),
+                    localNoSuchFieldException);
+        }
+        return null;
+    }
+}
